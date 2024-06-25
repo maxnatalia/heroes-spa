40977b8aa7dbeaf67b0a695bee286def6b3ce1d7
maxnatalia <Nataliamazur1988@gmail.com>
2024-06-25 08:01:48 +0200

    updated - added Navbar component

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,18 +1,21 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import MarvelPage from "../heroes/pages/MarvelPage";
-import DcPage from "../heroes/pages/DcPage";
-import LoginPage from "../auth/pages/LoginPage";
+import { DcPage, MarvelPage } from "../heroes";
+import { LoginPage } from "../auth";
+import { Navbar } from "../ui";
 
 const AppRouter = () => {
   return (
-    <Routes>
-      <Route path="marvel" element={<MarvelPage />} />
-      <Route path="dc" element={<DcPage />} />
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="marvel" element={<MarvelPage />} />
+        <Route path="dc" element={<DcPage />} />
 
-      <Route path="login" element={<LoginPage />} />
+        <Route path="login" element={<LoginPage />} />
 
-      <Route path="/" element={<Navigate to="/marvel" />} />
-    </Routes>
+        <Route path="/" element={<Navigate to="/marvel" />} />
+      </Routes>
+    </>
   );
 };
 
